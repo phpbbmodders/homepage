08aaba9e8fd8b760e98a9d02562c044f7182f9a2
William Jacoby <bonelifer@gmail.com>
2024-05-29 05:44:29 -0500

    initial commit

---
+++ language/en/website/homepage.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Language file for the phpBBModders homepage
+ * bonelifer (phpbbmodders.com)
+ */
+
+if (!defined('IN_PHPBB')) {
+    exit;
+}
+
+if (empty($lang) || !is_array($lang)) {
+    $lang = [];
+}
+
+// Define language variables
+$lang = array_merge($lang, array(
+    'L_LATEST_POSTS_HOMEPAGE' => 'Latest Posts',
+    'L_READ_TOPIC' => 'Read Topic',
+    'L_COMMENT' => 'Comment',
+    'L_COMMENTS' => 'Comments',
+    'L_COMMENT_TOPIC' => 'Comment on Topic',
+    'L_ANNOUNCEMENTS' => 'Announcements',
+));
+
+?>
+
